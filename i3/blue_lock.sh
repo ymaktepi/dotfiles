3f72ac54e98269ac171acecf198ce4ad55a0c6b7
Nicolas Huguenin <nichuguen@gmail.com>
2018-02-20 13:19:22 +0100

    added BSOD lock screen

---
+++ i3/blue_lock.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Lock screen displaying this image.
+i3lock -i ~/.config/i3/blue_lock.jpg --veriftext="" --insidevercolor=00000000 --insidewrongcolor=00000000 --insidecolor=00000000 --ringvercolor=00000000 --ringwrongcolor=00000000 --ringcolor=00000000 
+--linecolor=00000000 --keyhlcolor=000000ff --verifcolor=00000000 --wrongcolor=00000000
+
