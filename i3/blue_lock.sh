6ea8967f4d02ec8483b331b8a8ea3ce320d1a438
Nicolas Huguenin <nichuguen@gmail.com>
2018-02-21 09:26:18 +0100

    fixed blue_lock

--- i3/blue_lock.sh
+++ i3/blue_lock.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 # Lock screen displaying this image.
-i3lock -i ~/.config/i3/blue_lock.jpg --veriftext="" --insidevercolor=00000000 --insidewrongcolor=00000000 --insidecolor=00000000 --ringvercolor=00000000 --ringwrongcolor=00000000 --ringcolor=00000000 
---linecolor=00000000 --keyhlcolor=000000ff --verifcolor=00000000 --wrongcolor=00000000
+i3lock -i ~/.config/i3/blue_lock.jpg --veriftext="" --insidevercolor=00000000 --insidewrongcolor=00000000 --insidecolor=00000000 --ringvercolor=00000000 --ringwrongcolor=00000000 --ringcolor=00000000 --linecolor=00000000 --keyhlcolor=000000ff --verifcolor=00000000 --wrongcolor=00000000
 
