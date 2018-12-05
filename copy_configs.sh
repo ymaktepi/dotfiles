d7dfbf20051f00ecc47e38c28faf25cc55356b28
Nicolas Huguenin <nichuguen@gmail.com>
2018-12-05 22:45:54 +0100

    idempotent

--- copy_configs.sh
+++ copy_configs.sh
@@ -15,17 +15,25 @@ ln -s $(pwd)/terminator ~/.config/terminator
 rm -rf ~/.config/ranger
 ln -s $(pwd)/ranger ~/.config/ranger
 
+rm ~/.nanorc
 ln -s $(pwd)/nano/nanorc ~/.nanorc
+rm ~/.vimrc
 ln -s $(pwd)/vim/vimrc ~/.vimrc
 
+rm ~/Pictures/wallpaper.png
 ln -s $(pwd)/pictures/wallpaper.png ~/Pictures/wallpaper.png
 
+rm ~/.pylintrc
 ln -s $(pwd)/pylint/pylintrc ~/.pylintrc
 
+rm -rf ~/.shells
 ln -s $(pwd)/shells ~/.shells
 cp -n ~/.shells/customrc.example ~/.shells/customrc
+
+rm -rf ~/scripts
 ln -s $(pwd)/scripts ~/.scripts
 
 ln -s $(pwd)/xresources/Xresources ~/.Xresources
 
 sudo pacman -S yay
+yay -Syu
