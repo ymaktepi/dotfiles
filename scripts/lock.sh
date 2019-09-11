f233e3b54339d848aff52943d0c84f65c6c6c185
Nicolas Huguenin <nichuguen@gmail.com>
2019-09-11 13:30:38 +0200

    added override flag

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Take a screenshot
-scrot /tmp/screen_locked.png
+scrot /tmp/screen_locked.png -o
 
 # Pixellate it 10x (package imagemagick)
 mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
